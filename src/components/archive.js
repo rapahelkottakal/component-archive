f71b2260238ac678ca24caae0246925a3f014fb4
Raphael Kottakal <raphaelsemail@gmail.com>
2016-04-14 23:25:42 +0530

    Create a revel component and demo up the archive

--- src/components/archive.js
+++ src/components/archive.js
@@ -5,14 +5,16 @@ import 'normalize.css/normalize.css';
 import '../css/main.css';
 
 
+import Reveal from './reveal';
 import Title from './title';
+
 
 export default class Archive extends React.Component {
 
 	getContainerStyles() {
 		return {
 			minHeight: window.innerHeight,
-			backgroundColor: 'tomato',
+			// backgroundColor: 'tomato',
 			position: 'absolute',
 			top: 0,
 			left: 0,
@@ -28,29 +30,98 @@ export default class Archive extends React.Component {
 
 
 					<Title
-						text="Come home to a charm.."
+						text="React SIS Pages"
 						font= "Open Sans"
 						size="30"
 						color="white"
 						padding="20px"
 						align="right"
-						bgColor="gray"
+						bgColor="slateblue"
 						bgImage="http://fmforums.com/uploads/attachments/monthly_2009_10/Background-tile.gif.11107119f39d13d71f261c93ace94ed2.gif"
 					>
 
 						<Title 
-							text="Discover style"
+							text="We can do a lot more."
 							font= "Open Sans"
 							color="white"
 							size="16"
-							padding="0 10px 10px 10px"
+							padding="10px 0 0 0"
+							align="right"
+						/>
+						<Title 
+							text="This is only the beginning..."
+							font= "Open Sans"
+							color="white"
+							size="16"
+							padding="0px"
 							align="right"
 						/>
 						
 					</Title>
 
 
+					<Reveal
+						image="https://s-media-cache-ak0.pinimg.com/736x/b7/b5/be/b7b5be5c21678b1f53941f229c2b9cdd.jpg"
+						// btnTxt="Know more"
+						// btnSize={{width: 200, height: 100}}
+						// btnColor="red"
+						btnBgColor="hsla(0, 100%, 50%, 0.4)"
+						// content="The best collection of shoes can be found here."
+						// contentColor="yellow"
+						// contentBgColor="gray"
+						// shopBtnText="Click me now"
+						// shopLink="http://google.com"
 
+					/>
+
+					<img style={{display: 'block'}} src="http://assets.myntassets.com/v1460654675/city-tips-background_wy0nwb.jpg" class="alignnone wp-image-82898" width="100%" height="auto" />
+
+					<Title
+						text="Scalable"
+						font= "Open Sans"
+						size="30"
+						color="white"
+						padding="20px"
+						align="left"
+						bgColor="gray"
+						bgImage="http://templatesideas.com/wp-content/uploads/2010/12/Fiber-Carbon-Tiled-Pattern-background-vol.11.jpg"
+					>
+
+						<Title 
+							text="All these blocks are moduler. This means, making any improvements or adding more 'components' to the archive, will be simple for anyone familiar with reactjs."
+							font= "Open Sans"
+							color="white"
+							size="16"
+							padding="0px"
+							align="left"
+						/>
+						
+					</Title>
+
+					<Reveal
+						image="https://upload.wikimedia.org/wikipedia/commons/d/d2/Jeans_for_men.jpg"
+						btnTxt="Know more"
+						// btnSize={{width: 200, height: 100}}
+						// btnColor="red"
+						// btnBgColor="blue"
+						content="The best collection of jeans can be found here. What are you waiting for?"
+						// contentColor="yellow"
+						contentBgColor="hsla(230, 68%, 52%, 0.9)"
+						// shopBtnText="Click me now"
+						// shopLink="http://google.com"
+
+					/>
+
+					<Title
+						text="Helping you look good"
+						font= "Open Sans"
+						size="25"
+						color="white"
+						padding="20px"
+						// align="right"
+						bgColor="slateblue"
+						bgImage="http://www.uniteourresources.com/wp-content/uploads/2012/07/black_woven_seamless_tile.jpg"
+					/>
 
 				</div>
 			</div>
---
+++ src/components/reveal.js
@@ -0,0 +1,136 @@
+import React from 'react';
+
+import AbCenter from './abCenter';
+
+import animate from '../helpers/animate';
+
+export default class Title extends React.Component {
+
+	constructor() {
+		super();
+		this.state = {
+			expanded: false
+		}
+	}
+
+	getWrapperStyles() {
+
+		return {
+			backgroundColor: 'white',
+			minHeight: 200,
+			position: 'relative'
+		}
+	}
+
+	getImageStyles() {
+		return {
+			display: 'block',
+			width: '100%',
+
+		}
+	}
+
+	getBtnStyles() {
+
+		let width = this.props.btnSize && this.props.btnSize.width ? this.props.btnSize.width : 150,
+			height = this.props.btnSize && this.props.btnSize.height ? this.props.btnSize.height : 50,
+			btnColor = this.props.btnColor ? this.props.btnColor : 'white',
+			btnBgColor = this.props.btnBgColor ? this.props.btnBgColor : 'hsla(0, 0%, 0%, 0.8)',
+			contentColor = this.props.contentColor ? this.props.contentColor : 'white',
+			contentBgColor = this.props.contentBgColor ? this.props.contentBgColor : 'hsla(40, 100%, 15%, 0.9)';
+
+		let styles = {
+			fontSize: 18,
+			color: btnColor,
+			overflow: 'hidden',
+			backgroundColor: btnBgColor,
+			top: 0,
+			right: 0,
+			position: 'absolute',
+			width,
+			height,
+			cursor: 'pointer'
+		}
+
+		Object.assign(styles, animate.transition('0.5s'));
+
+		if (this.state.expanded) {
+			Object.assign(styles, {
+				color: contentColor,
+				backgroundColor: contentBgColor,
+				width: '100%',
+				height: '100%'
+			});
+		}
+
+		return styles;
+	}
+
+	getShopStyles() {
+		return {
+			display: 'inline-block',
+			backgroundColor: 'black',
+			color: 'white',
+			fontWeight: 600,
+			padding: '10px 15px',
+			marginTop: 20,
+			fontSize: 15,
+			textDecoration: 'none'
+
+		}
+	}
+
+	btnClickHandler() {
+		this.setState({
+			expanded: !this.state.expanded
+		});
+	}
+
+	hiddenRender() {
+
+		let btnTxt = this.props.btnTxt ? this.props.btnTxt : 'More...';
+
+		return (
+			<div style={this.getWrapperStyles()}>
+				<img style={this.getImageStyles()} src={this.props.image} />
+				<div style={this.getBtnStyles()} onClick={this.btnClickHandler.bind(this)}>
+					<AbCenter>
+						{btnTxt}
+					</AbCenter>
+				</div>
+			</div>
+		);
+
+	}
+
+	revealedRender() {
+		let content = this.props.content ? this.props.content : 'Shop on Myntra.com',
+			shopLink = this.props.shopLink ? this.props.shopLink : 'http://myntra.com/',
+			shopBtnText = this.props.shopBtnText ? this.props.shopBtnText : 'Shop Now';
+
+		return (
+			<div style={this.getWrapperStyles()}>
+				<img style={this.getImageStyles()} src={this.props.image} />
+				<div style={this.getBtnStyles()} onClick={this.btnClickHandler.bind(this)}>
+					<AbCenter padding="30">
+							{content}
+							<div>
+								<a style={this.getShopStyles()} href={shopLink} target="_blank">{shopBtnText}</a>
+							</div>
+					</AbCenter>
+
+				</div>
+			</div>
+		);
+	}
+
+	render() {
+
+
+		if (!this.state.expanded) {
+			return this.hiddenRender();
+		} else {
+			return this.revealedRender();
+		}
+	}
+}
