8d57a6f002a9ebf9d88508e4ecda95e29f2d6bb9
varun <varun.bondada@gmail.com>
2016-05-09 15:10:34 +0530

    add slider images

--- src/data/slider-group.js
+++ src/data/slider-group.js
@@ -1,72 +1,86 @@
 export default [
 	{
-		img: 'http://assets.myntassets.com/v1461231129/SIS/footwear-dictionary/B-background-min.jpg',
+		img: 'http://assets.myntassets.com/v1462785083/radium/SIS/footwear-heels/footwear-dictionary-B-min.jpg',
 		bgColor: '#F8F8F8',
 		slides: [
 			{
-				img: 'http://assets.myntassets.com/v1461231127/SIS/footwear-dictionary/brogues.png',
-				link: 'https://www.google.com'
-			},
-			{
-				img: 'http://assets.myntassets.com/v1461231127/SIS/footwear-dictionary/boat-shoes.png',
-				link: 'https://www.google.com'
-			},
-			{
-				img: 'http://assets.myntassets.com/v1461231127/SIS/footwear-dictionary/ballerinas.png',
-				link: 'https://www.google.com'
-			}
-		]
-	},
-	{
-		img: 'http://assets.myntassets.com/v1461231129/SIS/footwear-dictionary/B-background-min.jpg',
-		bgColor: '#F8F8F8',
-		slides: [
-			{
-				img: 'http://assets.myntassets.com/v1461231127/SIS/footwear-dictionary/boat-shoes.png',
-				link: 'https://www.google.com'
-			},
-			{
-				img: 'http://assets.myntassets.com/v1461231127/SIS/footwear-dictionary/brogues.png',
-				link: 'https://www.google.com'
-			},
-			{
-				img: 'http://assets.myntassets.com/v1461231127/SIS/footwear-dictionary/ballerinas.png',
-				link: 'https://www.google.com'
-			}
-		]
-	},
-	{
-		img: 'http://assets.myntassets.com/v1461231129/SIS/footwear-dictionary/B-background-min.jpg',
-		bgColor: '#F8F8F8',
-		slides: [
-			{
-				img: 'http://assets.myntassets.com/v1461231127/SIS/footwear-dictionary/ballerinas.png',
-				link: 'https://www.google.com'
-			},
-			{
-				img: 'http://assets.myntassets.com/v1461231127/SIS/footwear-dictionary/boat-shoes.png',
-				link: 'https://www.google.com'
-			},
-			{
-				img: 'http://assets.myntassets.com/v1461231127/SIS/footwear-dictionary/brogues.png',
+				img: 'http://assets.myntassets.com/v1462785770/radium/SIS/footwear-heels/Heels-block-heels-min.jpg',
 				link: 'https://www.google.com'
 			},
 		]
 	},
 	{
-		img: 'http://assets.myntassets.com/v1461231129/SIS/footwear-dictionary/B-background-min.jpg',
+		img: 'http://assets.myntassets.com/v1462785082/radium/SIS/footwear-heels/footwear-dictionary-F-min.jpg',
 		bgColor: '#F8F8F8',
 		slides: [
 			{
-				img: 'http://assets.myntassets.com/v1461231127/SIS/footwear-dictionary/ballerinas.png',
+				img: 'http://assets.myntassets.com/v1462785823/radium/SIS/footwear-heels/Heels-flatforms-min.jpg',
+				link: 'https://www.google.com'
+			},
+		]
+	},
+	{
+		img: 'http://assets.myntassets.com/v1462785083/radium/SIS/footwear-heels/footwear-dictionary-K-min.jpg',
+		bgColor: '#F8F8F8',
+		slides: [
+			{
+				img: 'http://assets.myntassets.com/v1462785904/radium/SIS/footwear-heels/Heels-kitten-heels-min.jpg',
+				link: 'https://www.google.com'
+			},
+		]
+	},
+	{
+		img: 'http://assets.myntassets.com/v1462785083/radium/SIS/footwear-heels/footwear-dictionary-M-min.jpg',
+		bgColor: '#F8F8F8',
+		slides: [
+			{
+				img: 'http://assets.myntassets.com/v1462785960/radium/SIS/footwear-heels/Heels-maryjanes-min.jpg',
+				link: 'https://www.google.com'
+			},
+		]
+	},
+	{
+		img: 'http://assets.myntassets.com/v1462785083/radium/SIS/footwear-heels/footwear-dictionary-P-min.jpg',
+		bgColor: '#F8F8F8',
+		slides: [
+			{
+				img: 'http://assets.myntassets.com/v1462786027/radium/SIS/footwear-heels/Heels-peeptoes-min.jpg',
 				link: 'https://www.google.com'
 			},
 			{
-				img: 'http://assets.myntassets.com/v1461231127/SIS/footwear-dictionary/boat-shoes.png',
+				img: 'http://assets.myntassets.com/v1462786031/radium/SIS/footwear-heels/Heels-platforms-min.jpg',
 				link: 'https://www.google.com'
 			},
 			{
-				img: 'http://assets.myntassets.com/v1461231127/SIS/footwear-dictionary/brogues.png',
+				img: 'http://assets.myntassets.com/v1462786070/radium/SIS/footwear-heels/Heels-pumps-min.jpg',
+				link: 'https://www.google.com'
+			},
+		]
+	},
+	{
+		img: 'http://assets.myntassets.com/v1462785083/radium/SIS/footwear-heels/footwear-dictionary-S-min.jpg',
+		bgColor: '#F8F8F8',
+		slides: [
+			{
+				img: 'http://assets.myntassets.com/v1462786174/radium/SIS/footwear-heels/Heels-sandsals-min.jpg',
+				link: 'https://www.google.com'
+			},
+			{
+				img: 'http://assets.myntassets.com/v1462786178/radium/SIS/footwear-heels/Heels-slingbacks-min.jpg',
+				link: 'https://www.google.com'
+			},
+			{
+				img: 'http://assets.myntassets.com/v1462786182/radium/SIS/footwear-heels/Heels-stilletos-min.jpg',
+				link: 'https://www.google.com'
+			},
+		]
+	},
+	{
+		img: 'http://assets.myntassets.com/v1462785082/radium/SIS/footwear-heels/footwear-dictionary-W-min.jpg',
+		bgColor: '#F8F8F8',
+		slides: [
+			{
+				img: 'http://assets.myntassets.com/v1462786278/radium/SIS/footwear-heels/Heels-wedges-min.jpg',
 				link: 'https://www.google.com'
 			},
 		]
