20ca3895d811c503e3de3b99ebf8580e2aca9f21
Raphael Kottakal <raphaelsemail@gmail.com>
2016-05-13 17:09:35 +0530

    Creat SIS page

--- src/components/archive.js
+++ src/components/archive.js
@@ -4,43 +4,42 @@ import Slider from 'react-slick';
 import 'normalize.css/normalize.css';
 import '../css/main.css';
 
-import Textonimage from './text-on-image'
-import Reveal from './reveal';
-import Title from './title';
-
-import Slideimage from './slideimage';
-import Popup from './popup';
-
-
-import FlipCard from './flip-card';
-import sliderGroupData from '../data/slider-group';
-import SliderGroup from './slide-group';
-
-import ShowMore from './showMore';
-import ImageChild from './imageChild';
-
 import Scroll from 'react-scroll';
 
 import { Link, Element, Events } from 'react-scroll';
 
-import HScroll from './h-scroll';
-
 import SplitSlider from './split-slider';
-
-import YouTube from 'react-youtube';
-
-import PinchZoom from './pinch-zoom';
 
 export default class Archive extends React.Component {
 
+	constructor() {
+		super();
+
+		let key, before, after, text;
+		key = 'Round';
+		before = "http://assets.myntassets.com/v1463130618/SIS/jewellery-faceshape/jewellery-face-shape-guide-round-night-min.jpg";
+		after = "http://assets.myntassets.com/v1463130618/SIS/jewellery-faceshape/jewellery-face-shape-guide-round-day-min.jpg";
+		text = "Add length to your face with lovely drop earrings. Go for long, angular designs that’ll give your face more definition and steer away from circular discs and hoops.";
+		this.state = {
+			key,
+			before,
+			after,
+			text,
+			split: 50
+
+		}
+	}
+
 	componentDidMount() {
 
+		this.setSplitter();
+
 		Events.scrollEvent.register('begin', function() {
-			console.log("begin", arguments);
+			// console.log("begin", arguments);
 		});
 
 		Events.scrollEvent.register('end', function() {
-			console.log("end", arguments);
+			// console.log("end", arguments);
 		});
 
 	}
@@ -55,7 +54,7 @@ export default class Archive extends React.Component {
 		return {
 			// overflowX: 'hidden',
 			minHeight: window.innerHeight,
-			// backgroundColor: 'tomato',
+			backgroundColor: '#B5B5B5',
 			position: 'absolute',
 			top: 0,
 			left: 0,
@@ -80,240 +79,153 @@ export default class Archive extends React.Component {
 		// event.target.pauseVideo();
 	}
 
+	setSplitter(event) {
+
+		let key, before, after, text;
+
+		if (event) {
+			key = event.target.innerHTML;
+			console.log(key);
+		}
+
+
+		switch(key) {
+			case 'Diamond':
+				before = "http://assets.myntassets.com/v1463136023/SIS/jewellery-faceshape/jewellery-face-shape-guide-diamond-night-min.jpg";
+				after = "http://assets.myntassets.com/v1463136022/SIS/jewellery-faceshape/jewellery-face-shape-guide-diamond-day-min.jpg";
+				text = "Long earrings with soft curves are great to soften the definition around the jawline. Steer clear of diamond shaped and other geometric danglers that don’t complement the angles of your face.";
+				break;
+			case 'Heart-shaped':
+				before = "http://assets.myntassets.com/v1463135946/SIS/jewellery-faceshape/jewellery-face-shape-guide-heart-night-min.jpg";
+				after = "http://assets.myntassets.com/v1463135946/SIS/jewellery-faceshape/jewellery-face-shape-guide-heart-day-min.jpg";
+				text = "Go for earrings with elongated curves that highlight your striking cheekbones. You might want to stay clear of hoops, tiny studs and other rounded designs that could make your face seem wider.";
+				break;
+			case 'Oval':
+				before = "http://assets.myntassets.com/v1463135809/SIS/jewellery-faceshape/jewellery-face-shape-guide-oval-night-min.jpg";
+				after = "http://assets.myntassets.com/v1463135809/SIS/jewellery-faceshape/jewellery-face-shape-guide-oval-day-min.jpg";
+				text = "You’re the luckiest one of the lot because you can wear all types of earrings! Earrings only enhance your face shape and make your features look sharper. Choose from a wide variety of studs as well as danglers.";
+				break;
+			case 'Square':
+				before = "http://assets.myntassets.com/v1463134616/SIS/jewellery-faceshape/jewellery-face-shape-guide-sqaure-night-min.jpg";
+				after = "http://assets.myntassets.com/v1463134616/SIS/jewellery-faceshape/jewellery-face-shape-guide-square-day-min.jpg";
+				text = "Go for rounded designs that soften your cheekbones and jawline. Stick to the narrower, slimmer varieties though, as wide ones might make your face seem broader.";
+				break;
+			default:
+				before = "http://assets.myntassets.com/v1463130618/SIS/jewellery-faceshape/jewellery-face-shape-guide-round-night-min.jpg";
+				after = "http://assets.myntassets.com/v1463130618/SIS/jewellery-faceshape/jewellery-face-shape-guide-round-day-min.jpg";
+				text = "Add length to your face with lovely drop earrings. Go for long, angular designs that’ll give your face more definition and steer away from circular discs and hoops.";
+				break;
+		}
+
+		this.setState({
+			key,
+			before,
+			after,
+			text,
+			split: 50
+		});
+		
+	}
+
+	renderSplitter() {
+		return (
+			<SplitSlider
+				before={this.state.before}
+				after={this.state.after}
+				split ={this.state.split}
+			/>
+		)
+	}
 
 	render() {
 
-		// console.log('test', sliderGroupData);
-
-
-		var settings = {
-			dots: true,
-			infinite: true,
-			speed: 250,
-			slidesToShow: 1,
-			slidesToScroll: 1,
-			autoplay: true,
-			autoplaySpeed: 3000,
-			centerMode: true,
-			arrows: false
-		};
-
 		let linkStyle = {
-			display: 'inline-block',
-			backgroundColor: 'tomato',
-			width: 100,
-			height: 50
+			display: 'table',
+			width: '100%',
+			height: 'auto'
 		};
 
-		const opts = {
-	      height: 'auto',
-	      width: '100%',
-	      playerVars: { // https://developers.google.com/youtube/player_parameters 
-	        // autoplay: 1,
-	        disablekb: 0,
-	        controls: 0,
-	        enablejsapi: 1,
-	        iv_load_policy: 3,
-	        modestbranding: 1,
-	        playsinline: 0,
-	        rel: 0,
-	        showinfo: 0
-	      }
-	    };
+		let thumb = {
+			display: 'table-cell',
+			width: '100%',
+			height: 'auto'
+		}
+
+		let thumbText = {
+			display: 'table-cell',
+			verticalAlign: 'middle',
+			paddingLeft: 32,
+			width: '70%',
+			fontSize: 16,
+			color: '#fff',
+			textTransform: 'uppercase'
+		}
+
+		let textStyle = {
+			backgroundColor: '#fff',
+			fontSize: 13,
+			lineHeight: '16px',
+			textAlign: 'center',
+			padding: '32px 16px',
+			color: '#525252'
+		}
+
+		let topStyle = {
+			display: 'block',
+			textAlign: 'center',
+			padding: 16,
+			textTransform: 'uppercase',
+			color: '#fff'
+		}
 
 		return(
 			<div style={this.getContainerStyles()}>
-
-				<HScroll childWidth={100}>
-					<Link activeClass="active" className="inactive" style={linkStyle} to="opener" spy={true} offset={-50} smooth={true} duration={500} >Opener</Link>
-					<Link activeClass="active" className="inactive" style={linkStyle} to="sliders" spy={true} offset={-50} smooth={true} duration={500} >Sliders</Link>
-					<Link activeClass="active" className="inactive" style={linkStyle} to="makeover" spy={true} offset={-50} smooth={true} duration={500} >Makeover</Link>
-					<Link activeClass="active" className="inactive" style={linkStyle} to="flipper" spy={true} offset={-50} smooth={true} duration={500} >Flipper</Link>
-					<Link activeClass="active" className="inactive" style={linkStyle} to="reveal" spy={true} offset={-50} smooth={true} duration={500} >Reveal</Link>
-				</HScroll>
-				<div style={{marginTop: 50}}>
-
-					<Element name="opener">
-					<ShowMore>
-						<ImageChild
-							image="http://assets.myntassets.com/v1462352818/radium/SIS/fragrance/fruity-menu.jpg"
-							width="33.33%"
-						>
-							<div>0 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ornare lacus et porttitor sollicitudin. Phasellus a eros ac risus elementum efficitur ac in mi. Vivamus dapibus, metus eu viverra scelerisque, sapien nisi cursus neque, eu venenatis erat lectus at enim.</div>
-
-						</ImageChild>
-						<ImageChild
-							image="http://assets.myntassets.com/v1462352816/radium/SIS/fragrance/floral-menu.jpg"
-							width="33.33%"
-						>
-							<div>1 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ornare lacus et porttitor sollicitudin. Phasellus a eros ac risus elementum efficitur ac in mi. Vivamus dapibus, metus eu viverra scelerisque, sapien nisi cursus neque, eu venenatis erat lectus at enim.</div>
+				<Element name="top" style={{marginBottom: 16}}>
+					<div style={{textAlign: 'center', color: '#fff', textTransform: 'uppercase', letterSpacing: 2, marginTop: 32}}>Choose your</div>
+					<div style={{textAlign: 'center', color: '#fff', textTransform: 'uppercase', letterSpacing: 2, fontSize: 32}}>Face shape</div>
+					<hr style={{borderTop: '1px solid #fff'}} width="30%" />
+					<Link style={linkStyle} to="active" spy={true} smooth={true} duration={500} onClick={this.setSplitter.bind(this)} >
+						<img src="http://assets.myntassets.com/v1463138629/SIS/jewellery-faceshape/jewellery-face-shape-guide-round-min.jpg"
+							alt="Round face shape"
 							
-						</ImageChild>
-						<ImageChild
-							image="http://assets.myntassets.com/v1462354162/radium/SIS/fragrance/oceanic-menu.jpg"
-							width="33.33%"
-						>
-							<div>2 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ornare lacus et porttitor sollicitudin. Phasellus a eros ac risus elementum efficitur ac in mi. Vivamus dapibus, metus eu viverra scelerisque, sapien nisi cursus neque, eu venenatis erat lectus at enim.</div>
-							
-						</ImageChild>
-					</ShowMore>
-					</Element>
-
-					<Element name="sliders">
-					<SliderGroup data={sliderGroupData}></SliderGroup>
-					</Element>
-
-					<Element name="makeover">
-					<Title
-						text="React SIS Pages"
-						font= "Open Sans"
-						size="30"
-						color="white"
-						padding="20px"
-						align="right"
-						bgColor="slateblue"
-						bgImage="http://fmforums.com/uploads/attachments/monthly_2009_10/Background-tile.gif.11107119f39d13d71f261c93ace94ed2.gif"
-					>
-
-						<Title 
-							text="We can do a lot more."
-							font= "Open Sans"
-							color="white"
-							size="16"
-							padding="10px 0 0 0"
-							align="right"
-						/>
-						<Title 
-							text="This is only the beginning..."
-							font= "Open Sans"
-							color="white"
-							size="16"
-							padding="0px"
-							align="right"
-						/>
+							style={thumb} />
+						<div style={thumbText}>Round</div>
+					</Link>
+					<Link style={linkStyle} to="active" spy={true} smooth={true} duration={500} onClick={this.setSplitter.bind(this)} >
+						<img src="http://assets.myntassets.com/v1463139489/SIS/jewellery-faceshape/jewellery-face-shape-guide-square-min.jpg"
+						alt="Square face shape"
 						
-					</Title>
+						style={thumb}/>
+						<div style={thumbText}>Square</div>
+					</Link>
+					<Link style={linkStyle} to="active" spy={true} smooth={true} duration={500} onClick={this.setSplitter.bind(this)} >
+						<img src="http://assets.myntassets.com/v1463138629/SIS/jewellery-faceshape/jewellery-face-shape-guide-oval-min.jpg"
+						alt="Oval face shape"
+						style={thumb}/>
+						<div style={thumbText}>Oval</div>
+					</Link>
+					<Link style={linkStyle} to="active" spy={true} smooth={true} duration={500} onClick={this.setSplitter.bind(this)} >
+						<img src="http://assets.myntassets.com/v1463138629/SIS/jewellery-faceshape/jewellery-face-shape-guide-heart-min.jpg"
+						alt="Heart face shape"
+						style={thumb}/>
+						<div style={thumbText}>Heart-shaped</div>
+					</Link>
+					<Link style={linkStyle} to="active" spy={true} smooth={true} duration={500} onClick={this.setSplitter.bind(this)} >
+						<img src="http://assets.myntassets.com/v1463138629/SIS/jewellery-faceshape/jewellery-face-shape-guide-diamond-min.jpg"
+						alt="Diamond face shape"
+						style={thumb}/>
+						<div style={thumbText}>Diamond</div>
+					</Link>
+				</Element>
 
-					<Slideimage
-					textonimage="Tap to be amazed"
-					beforeImage="http://assets.myntassets.com/v1461563423/Lookgood/Feed/makeover/april/Makeover-22.04.16--7.jpg"
-					afterImage="http://assets.myntassets.com/v1461563423/Lookgood/Feed/makeover/april/Makeover-22.04.16--8.jpg"
-					  />
+				<Element name="active">
+					{this.renderSplitter()}
+					<div style={textStyle}>{this.state.text}</div>
+				</Element>
 
-					<Popup 
-					content = "xxxx" />
-					</Element>
-
-					<Element name="flipper">
-
-					<FlipCard
-						front="http://assets.myntassets.com/v1461908504/Lookgood/Feed/Vocab/april/woh-cab-chevronprint-feedcard.gif"
-						back="http://assets.myntassets.com/v1461908455/Lookgood/Feed/Vocab/april/woh-cab-chevronprint.jpg"
-						link="http://www.myntra.com/chevron-print?userQuery=true&f=categories%3ADresses%2CKurtas%2CTops%3A%3Agender%3Amen%2520women%2Cmen%2520women%2Cmen%2520women%2Cmen%2520women%2Cmen%2520women%2Cwomen&sort=high&SRC=Lookgood6"
-					/>
-
-					</Element>
-
-
-					<Element name="reveal">
-					<Reveal
-						image="https://upload.wikimedia.org/wikipedia/commons/d/d2/Jeans_for_men.jpg"
-						btnTxt="Know more"
-						// btnSize={{width: 200, height: 100}}
-						// btnColor="red"
-						// btnBgColor="blue"
-						content="The best collection of jeans can be found here. What are you waiting for?"
-						// contentColor="yellow"
-						contentBgColor="hsla(230, 68%, 52%, 0.9)"
-						// shopBtnText="Click me now"
-						// shopLink="http://google.com"
-
-					/>
-					</Element>
-
-					<PinchZoom
-						image="http://assets.myntassets.com/v1463048534/radium/Lookbook/5.6/ethnic-look-1-image1.jpg"
-						ratio={1.45} />
-
-
-					<SplitSlider
-						before="http://assets.myntassets.com/v1461563423/Lookgood/Feed/makeover/april/Makeover-22.04.16--7.jpg" 
-						after="http://assets.myntassets.com/v1461563423/Lookgood/Feed/makeover/april/Makeover-22.04.16--8.jpg" />
-
-					<YouTube
-				        videoId="Yx9N57TbZiI"
-				        opts={opts}
-				        onReady={this._onReady}
-				      />
-
-					<Textonimage src ="http://assets.myntassets.com/v1460639223/Lookgood/Feed/Style-tips/april16/14/download.jpg"
-						text = "hi this is varun"
-						color = "red"
-						
-						>
-
-					</Textonimage>
-
-					<Textonimage src ="http://assets.myntassets.com/v1460639223/Lookgood/Feed/Style-tips/april16/14/download.jpg"
-						text = "hi thisasdfarun"
-						textPosition = "left"//center / left-top /right-bot
-						
-						>
-
-					</Textonimage>
-
-					<img style={{display: 'block'}} src="http://assets.myntassets.com/v1460654675/city-tips-background_wy0nwb.jpg" width="100%" height="auto" />
-
-					<Title
-						text="Scalable"
-						font= "Open Sans"
-						size="30"
-						color="white"
-						padding="20px"
-						align="left"
-						bgColor="gray"
-						bgImage="http://templatesideas.com/wp-content/uploads/2010/12/Fiber-Carbon-Tiled-Pattern-background-vol.11.jpg"
-					>
-
-						<Title 
-							text="All these blocks are modular. This means, making any improvements or adding more 'components' to the archive, will be simple for anyone familiar with reactjs."
-							font= "Open Sans"
-							color="white"
-							size="16"
-							padding="0px"
-							align="left"
-						/>
-						
-					</Title>
-
-					<Reveal
-						image="http://static1.squarespace.com/static/5520e514e4b00f468f8d3de4/5645f7a3e4b0abd1d824dfba/565780c9e4b022a250f9f480/1448575197974/cyclist-dribbble.gif"
-						// btnTxt="Know more"
-						// btnSize={{width: 200, height: 100}}
-						// btnColor="red"
-						btnBgColor="hsla(0, 100%, 50%, 0.4)"
-						// content="The best collection of shoes can be found here."
-						// contentColor="yellow"
-						// contentBgColor="gray"
-						// shopBtnText="Click me now"
-						// shopLink="http://google.com"
-
-					/>
-					
-
-					<Title
-						text="Helping you look good"
-						font= "Open Sans"
-						size="25"
-						color="white"
-						padding="20px"
-						// align="right"
-						bgColor="slateblue"
-						bgImage="http://www.uniteourresources.com/wp-content/uploads/2012/07/black_woven_seamless_tile.jpg"
-					/>
-
-
-				</div>
+				<Link style={topStyle} to="top" spy={true} smooth={true} duration={500} >
+					Go back
+				</Link>			
 				
 			</div>
 			
--- src/components/split-slider.js
+++ src/components/split-slider.js
@@ -3,29 +3,15 @@ import _ from 'lodash';
 
 export default class SplitSlider extends React.Component {
 
-	constructor() {
-		super();
+	constructor(props) {
+		super(props);
 		this.state = {
-			position: 50,
-			touche0x: null,
-			touche0y: null,
-			touche1x: null,
-			touche1y: null,
-			touche2x: null,
-			touche2y: null,
-			touche3x: null,
-			touche3y: null,
-			touche4x: null,
-			touche4y: null,
+			position: props.split
 
 		}
 	}
 
 	handleClick(e) {
-		// console.log('Clicked', e);
-		// console.log('X', e.clientX);
-		// console.log('Y', e.clientY);
-		// console.log('Width', e.target.parentElement.offsetWidth);
 
 		let width = e.target.parentElement.offsetWidth,
 			x = e.touches[0].clientX,
@@ -34,38 +20,8 @@ export default class SplitSlider extends React.Component {
 		// console.log(e.touches);
 
 		let setObject = {
-			position: spliterWidth,
-			touche0x: e.touches[0].clientX,
-			touche0y: e.touches[0].clientY,
+			position: spliterWidth
 
-		}
-
-		if (e.touches[1]) {
-			_.assignIn(setObject, {
-				touche1x: e.touches[1].clientX,
-				touche1y: e.touches[1].clientY
-			});
-		}
-
-		if (e.touches[2]) {
-			_.assignIn(setObject, {
-				touche2x: e.touches[2].clientX,
-				touche2y: e.touches[2].clientY
-			});
-		}
-
-		if (e.touches[3]) {
-			_.assignIn(setObject, {
-				touche3x: e.touches[3].clientX,
-				touche3y: e.touches[3].clientY
-			});
-		}
-
-		if (e.touches[4]) {
-			_.assignIn(setObject, {
-				touche4x: e.touches[4].clientX,
-				touche4y: e.touches[4].clientY
-			});
 		}
 
 		this.setState(setObject);
@@ -73,6 +29,12 @@ export default class SplitSlider extends React.Component {
 
 	handleMove(e) {
 		console.log('touch', e.touches[0].clientX);
+	}
+
+	resetPosition() {
+		this.setState({
+			position: 50
+		});
 	}
 
 	render() {
@@ -106,16 +68,6 @@ export default class SplitSlider extends React.Component {
 						<img style={imageStyle} src={this.props.before} />
 						<div style={afterImage}></div>
 					</div>
-					<div>T0 X - {this.state.touche0x}</div>
-					<div>T0 Y - {this.state.touche0y}</div>
-					<div>T1 X - {this.state.touche1x}</div>
-					<div>T1 Y - {this.state.touche1y}</div>
-					<div>T2 X - {this.state.touche2x}</div>
-					<div>T2 Y - {this.state.touche2y}</div>
-					<div>T3 X - {this.state.touche3x}</div>
-					<div>T3 Y - {this.state.touche3y}</div>
-					<div>T4 X - {this.state.touche4x}</div>
-					<div>T4 Y - {this.state.touche4y}</div>
 
 				</div>
 
