08a10e2179d9a80eedadada4c3a2e10d6fd66e88
Raphael Kottakal <raphaelsemail@gmail.com>
2016-05-17 12:04:03 +0530

    update images

--- src/data/slider-group.js
+++ src/data/slider-group.js
@@ -1,89 +1,132 @@
 export default [
 	{
-		img: 'http://assets.myntassets.com/v1463463743/radium/SIS/footwear-heels/b-min.jpg',
+		img: 'http://assets.myntassets.com/v1463465247/radium/SIS/footwear-flats/b-min.jpg',
 		bgColor: '#F8F8F8',
-		sliderSection: true,
 		slides: [
 			{
-				img: 'http://assets.myntassets.com/v1462785770/radium/SIS/footwear-heels/Heels-block-heels-min.jpg',
+				img: 'http://assets.myntassets.com/v1463465247/radium/SIS/footwear-flats/ballerinas-min.jpg',
+				link: 'https://www.google.com'
+			},
+			{
+				img: 'http://assets.myntassets.com/v1463465247/radium/SIS/footwear-flats/boat-shoes-min.jpg',
+				link: 'https://www.google.com'
+			},
+			{
+				img: 'http://assets.myntassets.com/v1463465247/radium/SIS/footwear-flats/brogues-min.jpg',
 				link: 'https://www.google.com'
 			},
 		]
 	},
 	{
-		img: 'http://assets.myntassets.com/v1463463743/radium/SIS/footwear-heels/f-min.jpg',
+		img: 'http://assets.myntassets.com/v1463465442/radium/SIS/footwear-flats/e-min.jpg',
 		bgColor: '#F8F8F8',
 		slides: [
 			{
-				img: 'http://assets.myntassets.com/v1462785823/radium/SIS/footwear-heels/Heels-flatforms-min.jpg',
+				img: 'http://assets.myntassets.com/v1463465443/radium/SIS/footwear-flats/espadrilles-min.jpg',
 				link: 'https://www.google.com'
 			},
 		]
 	},
 	{
-		img: 'http://assets.myntassets.com/v1463463743/radium/SIS/footwear-heels/k-min.jpg',
+		img: 'http://assets.myntassets.com/v1463465539/radium/SIS/footwear-flats/f-min.jpg',
 		bgColor: '#F8F8F8',
 		slides: [
 			{
-				img: 'http://assets.myntassets.com/v1462785904/radium/SIS/footwear-heels/Heels-kitten-heels-min.jpg',
+				img: 'http://assets.myntassets.com/v1463465539/radium/SIS/footwear-flats/flip-flops-min.jpg',
 				link: 'https://www.google.com'
 			},
 		]
 	},
 	{
-		img: 'http://assets.myntassets.com/v1463463743/radium/SIS/footwear-heels/m-min.jpg',
+		img: 'http://assets.myntassets.com/v1463466091/radium/SIS/footwear-flats/g-min.jpg',
 		bgColor: '#F8F8F8',
 		slides: [
 			{
-				img: 'http://assets.myntassets.com/v1462785960/radium/SIS/footwear-heels/Heels-maryjanes-min.jpg',
+				img: 'http://assets.myntassets.com/v1463466091/radium/SIS/footwear-flats/gladiators-min.jpg',
 				link: 'https://www.google.com'
 			},
 		]
 	},
 	{
-		img: 'http://assets.myntassets.com/v1463463743/radium/SIS/footwear-heels/p-min.jpg',
+		img: 'http://assets.myntassets.com/v1463466166/radium/SIS/footwear-flats/j-min.jpg',
 		bgColor: '#F8F8F8',
 		slides: [
 			{
-				img: 'http://assets.myntassets.com/v1462786027/radium/SIS/footwear-heels/Heels-peeptoes-min.jpg',
-				link: 'https://www.google.com'
-			},
-			{
-				img: 'http://assets.myntassets.com/v1462786031/radium/SIS/footwear-heels/Heels-platforms-min.jpg',
-				link: 'https://www.google.com'
-			},
-			{
-				img: 'http://assets.myntassets.com/v1462786070/radium/SIS/footwear-heels/Heels-pumps-min.jpg',
+				img: 'http://assets.myntassets.com/v1463466166/radium/SIS/footwear-flats/juttis-min.jpg',
 				link: 'https://www.google.com'
 			},
 		]
 	},
 	{
-		img: 'http://assets.myntassets.com/v1463463743/radium/SIS/footwear-heels/s-min.jpg',
+		img: 'http://assets.myntassets.com/v1463466216/radium/SIS/footwear-flats/l-min.jpg',
 		bgColor: '#F8F8F8',
 		slides: [
 			{
-				img: 'http://assets.myntassets.com/v1462786174/radium/SIS/footwear-heels/Heels-sandsals-min.jpg',
-				link: 'https://www.google.com'
-			},
-			{
-				img: 'http://assets.myntassets.com/v1462786178/radium/SIS/footwear-heels/Heels-slingbacks-min.jpg',
-				link: 'https://www.google.com'
-			},
-			{
-				img: 'http://assets.myntassets.com/v1462786182/radium/SIS/footwear-heels/Heels-stilletos-min.jpg',
+				img: 'http://assets.myntassets.com/v1463466216/radium/SIS/footwear-flats/loafers-min.jpg',
 				link: 'https://www.google.com'
 			},
 		]
 	},
 	{
-		img: 'http://assets.myntassets.com/v1463463744/radium/SIS/footwear-heels/w-min.jpg',
+		img: 'http://assets.myntassets.com/v1463466216/radium/SIS/footwear-flats/m-min.jpg',
 		bgColor: '#F8F8F8',
 		slides: [
 			{
-				img: 'http://assets.myntassets.com/v1462786278/radium/SIS/footwear-heels/Heels-wedges-min.jpg',
+				img: 'http://assets.myntassets.com/v1463466217/radium/SIS/footwear-flats/mocassins-min.jpg',
+				link: 'https://www.google.com'
+			},
+			{
+				img: 'http://assets.myntassets.com/v1463466217/radium/SIS/footwear-flats/monk-strap-boots-min.jpg',
 				link: 'https://www.google.com'
 			},
 		]
-	}
+	},
+	{
+		img: 'http://assets.myntassets.com/v1463466452/radium/SIS/footwear-flats/o-min.jpg',
+		bgColor: '#F8F8F8',
+		slides: [
+			{
+				img: 'http://assets.myntassets.com/v1463466452/radium/SIS/footwear-flats/one-toe-min.jpg',
+				link: 'https://www.google.com'
+			},
+		]
+	},
+	{
+		img: 'http://assets.myntassets.com/v1463466453/radium/SIS/footwear-flats/s-min.jpg',
+		bgColor: '#F8F8F8',
+		slides: [
+			{
+				img: 'http://assets.myntassets.com/v1463466453/radium/SIS/footwear-flats/slip-ons-min.jpg',
+				link: 'https://www.google.com'
+			},
+			{
+				img: 'http://assets.myntassets.com/v1463466454/radium/SIS/footwear-flats/sneakers-min.jpg',
+				link: 'https://www.google.com'
+			},
+			{
+				img: 'http://assets.myntassets.com/v1463466454/radium/SIS/footwear-flats/strappy-min.jpg',
+				link: 'https://www.google.com'
+			},
+		]
+	},
+	{
+		img: 'http://assets.myntassets.com/v1463466584/radium/SIS/footwear-flats/t-min.jpg',
+		bgColor: '#F8F8F8',
+		slides: [
+			{
+				img: 'http://assets.myntassets.com/v1463466584/radium/SIS/footwear-flats/t-straps-min.jpg',
+				link: 'https://www.google.com'
+			},
+		]
+	},
+	{
+		img: 'http://assets.myntassets.com/v1463466585/radium/SIS/footwear-flats/z-min.jpg',
+		bgColor: '#F8F8F8',
+		slides: [
+			{
+				img: 'http://assets.myntassets.com/v1463466585/radium/SIS/footwear-flats/zip-up-boots-min.jpg',
+				link: 'https://www.google.com'
+			},
+		]
+	},
 ];
