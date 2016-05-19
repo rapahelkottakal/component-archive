d14f4c216989ca59e28b54b506eeed18dd7c7ae1
Jagadesh <csejagadessh@gmail.com>
2016-05-19 16:52:20 +0530

    do the chges in the post based on the new mockup

--- src/components/archive.js
+++ src/components/archive.js
@@ -63,15 +63,63 @@ export default class Archive extends React.Component {
 						align="center"
 					/>	
 
-					<img style={{display: 'block'}} src="http://assets.myntassets.com/v1463562367/radium/SIS/lyla-loves/gold-arrow.jpg" width="100%" height="auto" />
+					<img style={{display: 'block'}} src="http://assets.myntassets.com/v1463655780/radium/SIS/lyla-loves/earrings.jpg" width="100%" height="auto" />
+					
+
+					<img style={{display: 'block', float:'left'}} src="http://assets.myntassets.com/v1463655839/radium/SIS/lyla-loves/ear-cuffs.jpg" width="50%" height="auto" />
+					<img style={{display: 'block', float:'left'}} src="http://assets.myntassets.com/v1463655839/radium/SIS/lyla-loves/drops.jpg" width="50%" height="auto" />
+					
+
+
+					<img style={{display: 'block'}} src="http://assets.myntassets.com/v1463655891/radium/SIS/lyla-loves/nekclaces.jpg" width="100%" height="auto" />
+
+					<img style={{display: 'block', marginBottom:'11%'}} src="http://assets.myntassets.com/v1463655923/radium/SIS/lyla-loves/necklaces-images.jpg" width="100%" height="auto" />
+
+
+					<img style={{display: 'block'}} src="http://assets.myntassets.com/v1463550131/radium/SIS/lyla-loves/Things-to-watch-out-for.jpg" width="100%" height="auto" />
+
+
+					<img style={{display: 'block'}} src="http://assets.myntassets.com/v1463550181/radium/SIS/lyla-loves/midi-rings.jpg" width="100%" height="auto" />
 					<Title 
-						text="The logo includes elements that represents a strong woman with a powerful sense of uniqueness. She has a dreamcatcher hand chain, butterflies in her hair, an elemental tattoo on her arm, arrows in her hand, turquoise head chain and feather earrings. Here’s what each one means."
+						text="Stylish and delicate, midi rings add a boho-chic touch to any look instantly.  "
+						font= 'Open Sans'
+						color="#494949"
+						size="15"
+						padding="1% 14% 9%"
+						align="left"
+					/>
+
+					<img style={{display: 'block'}} src="http://assets.myntassets.com/v1463550292/radium/SIS/lyla-loves/statement-rings.jpg" width="100%" height="auto" />
+					<Title 
+						text="Make a statement, literally! These neckpieces are a sure head-turner."
 						font= "Open Sans"
 						color="#494949"
 						size="15"
-						padding="3% 5% 9%"
-						align="center"
+						padding="1% 10% 9%"
+						align="right"
 					/>
+
+					<img style={{display: 'block'}} src="http://assets.myntassets.com/v1463550371/radium/SIS/lyla-loves/shoulder-duster.jpg" width="100%" height="auto" />
+					<Title 
+						text="These tasseled beauties will up your glam quotient by several notches. "
+						font= "Open Sans"
+						color="#494949"
+						size="15"
+						padding="1% 14% 9%"
+						align="left"
+					/>
+
+					<img style={{display: 'block'}} src="http://assets.myntassets.com/v1463550432/radium/SIS/lyla-loves/bracelets.jpg" width="100%" height="auto" />
+					<Title 
+						text="Dainty bracelets are all the rage. You can never go wrong with this one!"
+						font= "Open Sans"
+						color="#494949"
+						size="15"
+						padding="1% 10% 9%"
+						align="right"
+					/>
+
+
 
 					<Textonimage src ="http://assets.myntassets.com/v1463548260/radium/SIS/lyla-loves/butterflies.png"
 						text = "They signify change, rebirth and also new beginnings."
@@ -121,48 +169,7 @@ export default class Archive extends React.Component {
 						margin="-30% 0 0 0"
 					/>
 
-					<img style={{display: 'block'}} src="http://assets.myntassets.com/v1463550131/radium/SIS/lyla-loves/Things-to-watch-out-for.jpg" width="100%" height="auto" />
-
-
-					<img style={{display: 'block'}} src="http://assets.myntassets.com/v1463550181/radium/SIS/lyla-loves/midi-rings.jpg" width="100%" height="auto" />
-					<Title 
-						text="Stylish and delicate, midi rings add a boho-chic touch to any look instantly.  "
-						font= 'Open Sans'
-						color="#494949"
-						size="15"
-						padding="1% 14% 9%"
-						align="left"
-					/>
-
-					<img style={{display: 'block'}} src="http://assets.myntassets.com/v1463550292/radium/SIS/lyla-loves/statement-rings.jpg" width="100%" height="auto" />
-					<Title 
-						text="Make a statement, literally! These neckpieces are a sure head-turner."
-						font= "Open Sans"
-						color="#494949"
-						size="15"
-						padding="1% 10% 9%"
-						align="right"
-					/>
-
-					<img style={{display: 'block'}} src="http://assets.myntassets.com/v1463550371/radium/SIS/lyla-loves/shoulder-duster.jpg" width="100%" height="auto" />
-					<Title 
-						text="These tasseled beauties will up your glam quotient by several notches. "
-						font= "Open Sans"
-						color="#494949"
-						size="15"
-						padding="1% 14% 9%"
-						align="left"
-					/>
-
-					<img style={{display: 'block'}} src="http://assets.myntassets.com/v1463550432/radium/SIS/lyla-loves/bracelets.jpg" width="100%" height="auto" />
-					<Title 
-						text="Dainty bracelets are all the rage. You can never go wrong with this one!"
-						font= "Open Sans"
-						color="#494949"
-						size="15"
-						padding="1% 10% 9%"
-						align="right"
-					/>
+					
 					<a href="http://www.myntra.com/lyla-loves?SRC=Lookgood6"> 
 					<img style={{display: 'block', marginBottom: '10%'}} src="http://assets.myntassets.com/v1463556750/radium/SIS/lyla-loves/explore-collection.jpg" width="100%" height="auto" />
 					</a>
