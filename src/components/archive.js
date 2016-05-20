8a1a5bb7440ae71c366b0bdb5cd2f448663ea78d
Raphael Kottakal <raphaelsemail@gmail.com>
2016-05-20 14:25:35 +0530

    fix tiled background

--- src/components/archive.js
+++ src/components/archive.js
@@ -11,7 +11,7 @@ export default class Archive extends React.Component {
 	getContainerStyles() {
 		return {
 			minHeight: window.innerHeight,
-			backgroundImage: 'url(http://assets.myntassets.com/v1463475227/Lookgood/Feed/Look%20Book/may-16/17th/ethnic-look-book-printed-background.png)',
+			backgroundImage: 'url(http://assets.myntassets.com/v1463734373/radium/Lookbook/ethnic-look-book-printed-background.jpg)',
 			backgroundColor: 'black',
 			position: 'absolute',
 			top: 0,
