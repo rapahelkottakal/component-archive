65fe9048917e2876ecc1eb4fc8d684c310b75c9c
Raphael Kottakal <raphael@webmanifestation.com>
2016-06-01 12:36:29 +0530

    add window resize event and handler function

--- src/components/archive.js
+++ src/components/archive.js
@@ -6,15 +6,39 @@ import '../css/main.css';
 
 export default class Archive extends React.Component {
 
+	constructor() {
+		super();
+
+		this.state = {
+			windowHeight: window.innerHeight
+		};
+
+	}	
+
+	componentDidMount() {
+		window.addEventListener('resize', this.handleResize.bind(this));
+	}
+
+	componentWillUnmount() {
+		window.removeEventListener('resize', this.handleResize.bind(this));
+	}
+
+	handleResize(e) {
+		this.setState({
+			windowHeight: window.innerHeight
+		});
+	}
+
 	getContainerStyles() {
 		return {
-			minHeight: window.innerHeight,
+			minHeight: this.state.windowHeight,
 			position: 'absolute',
 			top: 0,
 			left: 0,
 			width: '100%',
 		}
 	}
+
 
 	render() {
 
