d6666189cdddca649cfe2c88493683886193386a
Raphael Kottakal <raphael@webmanifestation.com>
2016-06-06 13:37:34 +0530

    fix text, page title and move image load on componentDidMount

--- src/components/archive.js
+++ src/components/archive.js
@@ -8,6 +8,17 @@ import ImageChild from './imageChild';
 
 export default class Archive extends React.Component {
 
+	componentDidMount() {
+
+		const pastels = new Image,
+			blues = new Image,
+			reds = new Image;
+
+		pastels.src = 'http://assets.myntassets.com/v1465192044/radium/SIS/home-store/pastels.jpg';
+		blues.src = 'http://assets.myntassets.com/v1465192043/radium/SIS/home-store/blues-and-greens.jpg';
+		reds.src = 'http://assets.myntassets.com/v1465192046/radium/SIS/home-store/reds-and-maroons.jpg';
+	}
+
 
 	getContainerStyles() {
 		return {
@@ -22,15 +33,6 @@ export default class Archive extends React.Component {
 	}
 
 	render() {
-
-		let pastels = new Image,
-			blues = new Image,
-			reds = new Image;
-
-		pastels.src = 'http://assets.myntassets.com/v1465192044/radium/SIS/home-store/pastels.jpg';
-		blues.src = 'http://assets.myntassets.com/v1465192043/radium/SIS/home-store/blues-and-greens.jpg';
-		reds.src = 'http://assets.myntassets.com/v1465192046/radium/SIS/home-store/reds-and-maroons.jpg';
-
 
 		const css = {
 			banner: {
@@ -98,7 +100,7 @@ export default class Archive extends React.Component {
 						<div style={{
 							padding: '0 8px',
 							fontSize: 13
-						}}>Want your spaces to look stylish and contemporary? Here’s a simple guide that decodes colour pairing and helps you find the perfect bedsheets for your room. </div>
+						}}>Want your spaces to look stylish and contemporary? Here’s a simple guide that decodes colour pairing and helps you find the perfect bedsheets for your room.</div>
 					</div>
 
 					<div style={{
@@ -123,7 +125,7 @@ export default class Archive extends React.Component {
 									borderBottom: '1px solid #bca38a',
 									marginBottom: 8
 								}}></div>
-								<div style={css.text}>All colours</div>
+								<div style={css.text}>All colours including bright hues like fuchsia or turquoise blue, darker shades like blacks and browns and muted tones like pastels.</div>
 
 								<div style={css.title}><span style={{color: '#bca38a'}}>Style</span></div>
 								<div style={{
@@ -132,8 +134,8 @@ export default class Archive extends React.Component {
 									marginBottom: 8
 								}}></div>
 								<div style={css.text}>
-									<strong>Experimental</strong>-  Bed sheets in bold hues and pop colours with ethnic or quirky prints<br/><br/>
-									<strong>Minimal chic</strong>- Subtle pastels or floral prints 
+									<strong>Experimental</strong>-  Make a statement by choosing sheets in a bright colour with an ethnic all-over print that adds a certain artsy touch to your room.<br/><br/>
+									<strong>Minimal chic</strong>- Elegant floral prints on sheets with a cream or white base are sure to add a charming touch. If florals aren't your thing, go for minimalistic stripes in black and white that are always on trend.
 								</div>
 
 								<div style={css.title}><span style={{color: '#bca38a'}}>Tips</span></div>
@@ -160,7 +162,7 @@ export default class Archive extends React.Component {
 									borderBottom: '1px solid #8eb8ba',
 									marginBottom: 8
 								}}></div>
-								<div style={css.text}>Sea greens, pastels, Blues, bright pinks, bright oranges, yellows</div>
+								<div style={css.text}>Sea greens, pastels, blues, bright pinks, bright oranges, yellows.</div>
 
 								<div style={css.title}><span style={{color: '#8eb8ba'}}>Style</span></div>
 								<div style={{
@@ -169,8 +171,8 @@ export default class Archive extends React.Component {
 									marginBottom: 8
 								}}></div>
 								<div style={css.text}>
-									<strong>Experimental</strong>-  Pairing pale blue walls with bright sea greens works wonders to add contrast<br/><br/>
-									<strong>Minimal chic</strong>- If you want to keep the tone of your room minimalistic go for bed sheets in the same colour family as the predominant room colour
+									<strong>Experimental</strong>-  Pairing pale blue walls with bright sea greens works wonders to add a touch of contrast.<br/><br/>
+									<strong>Minimal chic</strong>- If you want to keep the tone of your room minimalistic go for bed sheets in the same colour family as the predominant room colour.
 								</div>
 
 								<div style={css.title}><span style={{color: '#8eb8ba'}}>Tips</span></div>
@@ -179,7 +181,7 @@ export default class Archive extends React.Component {
 									borderBottom: '1px solid #8eb8ba',
 									marginBottom: 8
 								}}></div>
-								<div style={css.text}>When you have predominantly light shades in your room you can pair them with hues that are starkly in contrast to add depth to your room.</div>
+								<div style={css.text}>When you have predominantly light shades in your room you can pair them with hues that are starkly in contrast to add depth to the space.</div>
 							</div>
 							<a href="http://www.myntra.com/bedsheets?userQuery=true&f=colour%3Ablue%2Ccoral%2Clavender%2Corange%2Cpeach%2Cpink%2Csea%2520green%2Cyellow&SRC=Radium" target="_blank" style={css.shop}>Shop collection</a>
 							
@@ -197,7 +199,7 @@ export default class Archive extends React.Component {
 									borderBottom: '1px solid #50c2b2',
 									marginBottom: 8
 								}}></div>
-								<div style={css.text}>Creams, whites, blues, greens</div>
+								<div style={css.text}>Creams, whites, blues, greens.</div>
 
 								<div style={css.title}><span style={{color: '#50c2b2'}}>Style</span></div>
 								<div style={{
@@ -206,8 +208,8 @@ export default class Archive extends React.Component {
 									marginBottom: 8
 								}}></div>
 								<div style={css.text}>
-									<strong>Experimental</strong>-  Match the colour of your sheets to the predominant room colours<br/><br/>
-									<strong>Minimal chic</strong>- Tone down bright colours with neutral-coloured sheets in creams and whites with geometric prints or stripes 
+									<strong>Experimental</strong>-  Match the colour of your sheets to the predominant room colours.<br/><br/>
+									<strong>Minimal chic</strong>- Tone down bright colours with neutral-coloured sheets in creams and whites with geometric prints or stripes.
 								</div>
 
 								<div style={css.title}><span style={{color: '#50c2b2'}}>Tips</span></div>
@@ -234,7 +236,7 @@ export default class Archive extends React.Component {
 									borderBottom: '1px solid #c8454f',
 									marginBottom: 8
 								}}></div>
-								<div style={css.text}>Whites, greys, creams, blacks</div>
+								<div style={css.text}>Whites, greys, creams, blacks.</div>
 
 								<div style={css.title}><span style={{color: '#c8454f'}}>Style</span></div>
 								<div style={{
@@ -243,8 +245,8 @@ export default class Archive extends React.Component {
 									marginBottom: 8
 								}}></div>
 								<div style={css.text}>
-									<strong>Experimental</strong>-  Go for black and white stripes or geometric prints. Play it up with ethnic prints in complementary colours in the red family<br/><br/>
-									<strong>Minimal chic</strong>- Pair the bright reds or deep maroons with greys, creams and whites
+									<strong>Experimental</strong>-  Go for black and white stripes or geometric prints. Play it up with ethnic prints in complementary colours from the red family.<br/><br/>
+									<strong>Minimal chic</strong>- Pair the bright reds or deep maroons with greys, creams and whites.
 								</div>
 
 								<div style={css.title}><span style={{color: '#c8454f'}}>Tips</span></div>
