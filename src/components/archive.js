225a122b130f4e330d54c4a0c7a5eef6d3d37e86
Raphael Kottakal <raphaelsemail@gmail.com>
2016-06-09 16:41:14 +0530

    create bottom flip div

--- src/components/archive.js
+++ src/components/archive.js
@@ -19,13 +19,51 @@ export default class Archive extends React.Component {
 
 	render() {
 
+		const css = {
+			bottomFold: {
+				container: {
+					marginTop: 16,
+					position: 'relative'
+				},
+				text: {
+					backgroundColor: '#FFADB9',
+					textAlign: 'center',
+					padding: 16,
+					borderTopLeftRadius: 25,
+					borderTopRightRadius: 25,
+					margin: '0 27px',
+					fontSize: 13,
+					lineHeight: 1.5
+				},
+				rightCorner: {
+					position: 'absolute',
+					bottom: 0,
+					right: 0
+				},
+				leftCorner: {
+					transform: 'scaleX(-1)',
+					position: 'absolute',
+					bottom: 0,
+					left: 0
+				}
+			}	
+		}
+
 		return(
 			<div style={this.getContainerStyles()}>
-
 				<div>
 
+					<div style={css.bottomFold.container}>
+						<div style={css.bottomFold.text}>Wash your bras after every 2-3 wears and always use a gentle alcohol-free detergent that’s less harsh on the fabric.</div>
+						<img style={css.bottomFold.rightCorner} src="http://assets.myntassets.com/v1465468864/SIS/bra-guide/corner2.jpg" />
+						<img style={css.bottomFold.leftCorner} src="http://assets.myntassets.com/v1465468864/SIS/bra-guide/corner2.jpg" />
+					</div>
+					<div style={css.bottomFold.container}>
+						<div style={css.bottomFold.text}>Wash your bras after every 2-3 wears and always use a gentle alcohol-free detergent that’s less harsh on the fabric.</div>
+						<img style={css.bottomFold.rightCorner} src="http://assets.myntassets.com/v1465468864/SIS/bra-guide/corner2.jpg" />
+						<img style={css.bottomFold.leftCorner} src="http://assets.myntassets.com/v1465468864/SIS/bra-guide/corner2.jpg" />
+					</div>
 				</div>
-				
 			</div>			
 		);
 
