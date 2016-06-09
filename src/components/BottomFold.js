87c924935f6eac12c5868a535d252baf7004498a
Raphael Kottakal <raphaelsemail@gmail.com>
2016-06-09 16:56:56 +0530

    make a seperate component BottomFold

---
+++ src/components/BottomFold.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+export default class BottomFold extends React.Component {
+
+	render() {
+
+		const css = {
+			container: {
+				marginTop: 16,
+				position: 'relative'
+			},
+			text: {
+				backgroundColor: this.props.backgroundColor,
+				textAlign: 'center',
+				padding: 16,
+				borderTopLeftRadius: 25,
+				borderTopRightRadius: 25,
+				margin: '0 27px',
+				fontSize: 13,
+				lineHeight: 1.5
+			},
+			rightCorner: {
+				position: 'absolute',
+				bottom: 0,
+				right: 0
+			},
+			leftCorner: {
+				transform: 'scaleX(-1)',
+				position: 'absolute',
+				bottom: 0,
+				left: 0
+			}	
+		};
+
+		return(
+			<div style={css.container}>
+				<div style={css.text}>{this.props.text}</div>
+				<img style={css.rightCorner} src={this.props.cornerImage} />
+				<img style={css.leftCorner} src={this.props.cornerImage} />
+			</div>
+		);
+	}
+}
