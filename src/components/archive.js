45b52f5975e6f6b5dda4cd90765aaa05bad857a7
Raphael Kottakal <raphaelsemail@gmail.com>
2016-07-05 18:58:51 +0530

    update links and add ga

--- src/components/archive.js
+++ src/components/archive.js
@@ -10,6 +10,8 @@ import { Link, Element, Events } from 'react-scroll';
 
 import SplitSlider from './split-slider';
 
+import Ga from '../helpers/Ga';
+
 export default class Archive extends React.Component {
 
 	constructor() {
@@ -66,6 +68,12 @@ export default class Archive extends React.Component {
 		}
 	}
 
+	handelShopBtnClick(e) {
+
+		// console.log(e.target.closest('a'));
+		Ga({ label: e.target.closest('a').href });
+	}
+
 
 	// getTextPositionStyles() {
 	// 	return {
@@ -106,40 +114,40 @@ export default class Archive extends React.Component {
 				before = "http://assets.myntassets.com/v1463469185/radium/SIS/jewellery-faceshape/jewellery-face-shape-guide-diamond-night-min.jpg";
 				after = "http://assets.myntassets.com/v1463469185/radium/SIS/jewellery-faceshape/jewellery-face-shape-guide-diamond-day-min.jpg";
 				text = "Long earrings with soft curves are great to soften the definition around the jawline. Steer clear of diamond shaped and other geometric danglers that don’t complement the angles of your face.";
-				dayLink = "http://www.myntra.com/earrings?f=Type_article_attr%3Aear%2520cuff&SRC=Radium";
-				nightLink = "http://www.myntra.com/shoulder-duster-earrings?SRC=Radium";
+				dayLink = "http://www.myntra.com/jewellery-guide?f=Type_article_attr%3Aear%2520cuff%3A%3Acategories%3AEarrings&SRC=Radium";
+				nightLink = "http://www.myntra.com/jewellery-guide-shoulder-duster-earrings?SRC=Radium";
 				keyTitle = "Diamond-shaped face"
 				break;
 			case 'Heart':
 				before = "http://assets.myntassets.com/v1463469185/radium/SIS/jewellery-faceshape/jewellery-face-shape-guide-heart-night-min.jpg";
 				after = "http://assets.myntassets.com/v1463469185/radium/SIS/jewellery-faceshape/jewellery-face-shape-guide-heart-day-min.jpg";
 				text = "Go for earrings with elongated curves that highlight your striking cheekbones. You might want to stay clear of hoops, tiny studs and other rounded designs that could make your face seem wider.";
-				dayLink = "http://www.myntra.com/drop-earrings-for-day?SRC=Radium";
-				nightLink = "http://www.myntra.com/earrings?f=Type_article_attr%3Ajhumkas&SRC=Radium";
+				dayLink = "http://www.myntra.com/jewellery-guide-drop-earrings-for-day?SRC=Radium";
+				nightLink = "http://www.myntra.com/jewellery-guide?f=Type_article_attr%3Ajhumkas%3A%3Acategories%3AEarrings&SRC=Radium";
 				keyTitle = "Heart-shaped face"
 				break;
 			case 'Oval':
 				before = "http://assets.myntassets.com/v1463469185/radium/SIS/jewellery-faceshape/jewellery-face-shape-guide-oval-night-min.jpg";
 				after = "http://assets.myntassets.com/v1463469185/radium/SIS/jewellery-faceshape/jewellery-face-shape-guide-oval-day-min.jpg";
 				text = "You’re the luckiest one of the lot because you can wear all types of earrings! Earrings only enhance your face shape and make your features look sharper. Choose from a wide variety of studs as well as danglers.";
-				dayLink = "http://www.myntra.com/earrings?f=Type_article_attr%3Astuds&SRC=Radium";
-				nightLink = "http://www.myntra.com/earrings?f=Type_article_attr%3Achandbali$SRC=Radium";
+				dayLink = "http://www.myntra.com/jewellery-guide?f=Type_article_attr%3Astuds%3A%3Acategories%3AEarrings&SRC=Radium";
+				nightLink = "http://www.myntra.com/jewellery-guide?f=Type_article_attr%3Achandbali%3A%3Acategories%3AEarrings&SRC=Radium";
 				keyTitle = "Oval face"
 				break;
 			case 'Square':
 				before = "http://assets.myntassets.com/v1463469186/radium/SIS/jewellery-faceshape/jewellery-face-shape-guide-sqaure-night-min.jpg";
 				after = "http://assets.myntassets.com/v1463469186/radium/SIS/jewellery-faceshape/jewellery-face-shape-guide-square-day-min.jpg";
 				text = "Go for rounded designs that soften your cheekbones and jawline. Stick to the narrower, slimmer varieties though, as wide ones might make your face seem broader.";
-				dayLink = "http://www.myntra.com/thin-hoops-fora-day?SRC=Radium";
-				nightLink = "http://www.myntra.com/slim-danglers-for-night?SRC=Radium";
+				dayLink = "http://www.myntra.com/jewellery-guide-thin-hoops-fora-day?SRC=Radium";
+				nightLink = "http://www.myntra.com/jewellery-guide-slim-danglers-for-night?SRC=Radium";
 				keyTitle = "Square face"
 				break;
 			default:
 				before = "http://assets.myntassets.com/v1463469186/radium/SIS/jewellery-faceshape/jewellery-face-shape-guide-round-night-min.jpg";
 				after = "http://assets.myntassets.com/v1463469186/radium/SIS/jewellery-faceshape/jewellery-face-shape-guide-round-day-min.jpg";
 				text = "Add length to your face with lovely drop earrings. Go for long, angular designs that’ll give your face more definition and steer away from circular discs and hoops.";
-				dayLink = "http://www.myntra.com/tear-drop-earings-for-day?SRC=Radium";
-				nightLink = "http://www.myntra.com/chandelier-earrings-for-night?SRC=Radium";
+				dayLink = "http://www.myntra.com/jewellery-guide-tear-drop-earings-for-day?SRC=Radium";
+				nightLink = "http://www.myntra.com/jewellery-guide-chandelier-earrings-for-night?SRC=Radium";
 				keyTitle = "Round face"
 				break;
 		}
@@ -292,10 +300,10 @@ export default class Archive extends React.Component {
 					<div style={titleStyle}>{this.state.keyTitle}</div>
 					{this.renderSplitter()}
 					<div style={shopBtnWrapper}>
-						<a target="_blank" href={this.state.dayLink} style={shopBtn}>Shop <strong>day</strong></a>
+						<a onClick={this.handelShopBtnClick} target="_blank" href={this.state.dayLink} style={shopBtn}>Shop <strong>day</strong></a>
 					</div>
 					<div style={shopBtnWrapper}>
-						<a target="_blank" href={this.state.nightLink} style={shopBtn}>Shop <strong>night</strong></a>
+						<a onClick={this.handelShopBtnClick} target="_blank" href={this.state.nightLink} style={shopBtn}>Shop <strong>night</strong></a>
 					</div>
 					<div style={textStyle}>{this.state.text}</div>
 				</Element>
